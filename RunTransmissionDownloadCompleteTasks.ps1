e708b22cba6e1e1068436155bd1f118334bfdf42
Me <Myself@nowhere.where>
2016-01-14 03:18:42 +0100

    Initial commit

---
+++ RunTransmissionDownloadCompleteTasks.ps1
@@ -0,0 +1,19 @@
+﻿function StartTransmissionDownloadCompleteTasks ([Parameter(Mandatory=$True)][String]$WorkingDirectory)
+{
+	Get-Childitem -Path $WorkingDirectory -Filter *.ps1	| % {& $_.FullName}
+	Get-Childitem -Path $WorkingDirectory -Filter *.bat	| % {& $_.FullName}
+	Get-Childitem -Path $WorkingDirectory -Filter *.py	| % {& $_.FullName}
+	Get-Childitem -Path $WorkingDirectory -Filter *.rb	| % {& $_.FullName}
+}
+
+If ($env:TransmissionScripts -ne $null ) {
+	StartTransmissionDownloadCompelteTasks $env:TransmissionScripts
+}
+If ("$env:LOCALAPPDATA\transmission\scripts" -ne $null) {
+	StartTransmissionDownloadCompleteTasks $env:LOCALAPPDATA\transmission\scripts
+}
+If ("$env:APPDATA\transmission\scripts" -ne $null) {
+	StartTransmissionDownloadCompleteTasks $env:APPDATA\transmission\scripts
+}
+
+$TR_TORRENT_DIR > $env:TMP/transmission_test.txt
